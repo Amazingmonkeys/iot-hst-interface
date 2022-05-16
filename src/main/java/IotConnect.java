09a189561266d98bc94f7060a1e4ef09c05367e2
RenXinyuGit <2451360621@qq.com>
2022-05-16 11:54:48 +0800

    注释

--- src/main/java/IotConnect.java
+++ src/main/java/IotConnect.java
@@ -3,6 +3,10 @@ public class IotConnect {
     private static final String URL_ADDRESS = "http://47.94.163.75:8080/iot-hst";
 
     // /N2MjzRBSyeMMQEnb/BAYenxJcfQMNY4MCWHSYEjN6/218.108.98.98/10720/CMS-CA-80765
+
+    /**
+     * 配置5个变量
+     */
     public static void setConfig(String clientId, String secretKey, String serverHost, String serverPort,
                                  String deviceName) {
         String result = HttpUtil.httpPost("{}", URL_ADDRESS + "/config/" + clientId + "/" + secretKey + "/"
@@ -10,31 +14,54 @@ public class IotConnect {
         System.out.println(result);
     }
 
-    public static void a001() {
-        String result = HttpUtil.httpGet(URL_ADDRESS + "/A001");
-        System.out.println(result);
+    /**
+     * 获取当前显示内容
+     *
+     * @return result
+     */
+    public static String a001() {
+        return HttpUtil.httpGet(URL_ADDRESS + "/A001");
     }
 
-    public static void a002() {
-        String result = HttpUtil.httpGet(URL_ADDRESS + "/A002");
-        System.out.println(result);
+    /**
+     * 获取亮度和调节方式
+     *
+     * @return result
+     */
+    public static String a002() {
+        return HttpUtil.httpGet(URL_ADDRESS + "/A002");
     }
 
-    public static void a003() {
-        String result = HttpUtil.httpGet(URL_ADDRESS + "/A003");
-        System.out.println(result);
+    /**
+     * 下载节目列表
+     *
+     * @return result
+     */
+    public static String a003() {
+        return HttpUtil.httpGet(URL_ADDRESS + "/A003");
     }
 
-    public static void b001(String str) {
+    /**
+     * 设置显示内容
+     *
+     * @param str: 显示内容
+     * @return {"result":["ok"],"status":200,"code":"success"}
+     */
+    public static String b001(String str) {
         String jsonStr = "{\"data\":{\"items\":[{\"transition\":1,\"delay\":500,\"param\":0,\"imgCode\":\"\",\"str\"" +
                 ":\"" + str + "\",\"color\":\"red\",\"fonttype\":\"s\",\"fontsize\":24,\"charSpacing\": 10}]}}";
-        String result = HttpUtil.httpPost(jsonStr, URL_ADDRESS + "/B001");
-        System.out.println(result);
+        return HttpUtil.httpPost(jsonStr, URL_ADDRESS + "/B001");
     }
 
-    public static void b002(Integer method, Integer light) {
+    /**
+     * 设置亮度和调节方式
+     *
+     * @param method: 调节方式
+     * @param light:  亮度
+     * @return {"result":["ok"],"status":200,"code":"success"}
+     */
+    public static String b002(Integer method, Integer light) {
         String jsonStr = "{\"data\": {\"method\":" + method + ",\"light\":" + light + "}}";
-        String result = HttpUtil.httpPost(jsonStr, URL_ADDRESS + "/B002");
-        System.out.println(result);
+        return HttpUtil.httpPost(jsonStr, URL_ADDRESS + "/B002");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         IotConnect.setConfig("N2MjzRBSyeMMQEnb", "BAYenxJcfQMNY4MCWHSYEjN6", "218.108.98.98",
                 "10720", "CMS-CA-80765");
+        System.out.println(IotConnect.a001());
     }
 }
